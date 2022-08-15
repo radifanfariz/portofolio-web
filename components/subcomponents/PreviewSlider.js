280eb61ed93323807691c3a5039a39d7dfb1f0a2
Faiz <radifanfariz98@gmail.com>
2022-08-15 22:44:53 +0700

    Update Preview Slider Height and Width Setting

--- components/subcomponents/PreviewSlider.js
+++ components/subcomponents/PreviewSlider.js
@@ -22,14 +22,14 @@ const uniqueId = () => {
 const PreviewSlider = ({ imageList }) => {
     return (
         <div className="mx-3">
-            <Swiper key={uniqueId} navigation={true} modules={[Navigation]} spaceBetween={-350} slidesPerView={2} className="mySwiper rounded-[100px] border-4">
+            <Swiper key={uniqueId} navigation={true} modules={[Navigation]} spaceBetween={-50} slidesPerView={2} className="mySwiper rounded-[100px] border-4">
                 {
                     imageList.map((images) =>
                         images.src.map((item) => {
                             console.log(images)
                             return (
                                 <SwiperSlide key={uniqueId()}>
-                                    <div key={uniqueId()} className='w-[200px] h-[400px] relative ml-10 '>
+                                    <div key={uniqueId()} className='w-[350px] h-[800px] relative ml-10 '>
                                         <Image key={uniqueId()} className='' layout='fill' src={item} alt='template' />
                                         <div key={uniqueId()} className="hover:opacity-50 opacity-0 flex text-3xl relative justify-center text-center items-center h-full font-semibold hover:bg-black hover:bg-opacity-25 rounded-3xl">{images.title}</div>
                                     </div>
